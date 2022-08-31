b0649be04a9464f22e7cfb67f797e83c64583918
vishal <vishalambekar459@gmail.com>
2022-08-31 23:47:58 +0530

    initial code added

---
+++ CSV_TO_JAVA_CONVERTER/src/main/java/io/vishal/CsvToJavaConverterApplication.java
@@ -0,0 +1,13 @@
+package io.vishal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvToJavaConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvToJavaConverterApplication.class, args);
+	}
+
+}
